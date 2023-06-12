83d2e73bd1daa880ad195582aabadbd255fa6dc8
kpoter <kpoter@mkone.co.kr>
2023-06-12 20:41:43 +0900

    초기 커밋

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import './test.scss';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class Menu extends Component {
 
-export default App;
+
+  state = {
+    click: false,
+  }
+
+  onClick = () => {
+    this.setState({ click: !this.state.click })
+  }
+
+  render() {
+    const lvl = { value1: 100, value2: 200, value3: 300, value4: 150, value5: 270 };
+    return (
+      <>
+        <div className="App">
+          <p className='info'>안녕</p>
+          <button className='ml5' onClick={this.onClick}>입력</button>
+          {
+            !this.state.click &&
+            <>
+              <p className='lh500'>안녕</p>
+              <div className='mr500'>안녕하시오</div>
+            </>
+          }
+        </div>
+        {
+          this.state.click &&
+          <div className='table1 ml500'>
+            <table>
+              <tr>
+                <th>레벨</th>
+                <th>아이디</th>
+              </tr>
+              <tr>
+                <td>{lvl.value1}</td>
+                <td>react</td>
+              </tr>
+              <tr>
+                <td>{lvl.value2}</td>
+                <td>vue</td>
+              </tr>
+              <tr>
+                <td>{lvl.value3}</td>
+                <td>spring</td>
+              </tr>
+              <tr>
+                <td>{lvl.value4}</td>
+                <td>customer</td>
+              </tr>
+              <tr>
+                <td>{lvl.value5}</td>
+                <td>angular</td>
+              </tr>
+            </table>
+          </div>
+        }
+      </>
+    );
+  }
+}
